c44e6a70a23dde4ebaffab8156eca76268e6e650
Samuel <KremplS21084@sus.htl-grieskirchen.at>
2024-09-17 14:38:21 +0200

    commit 1 & 2& 3

---
+++ untitled/src/EratosthenesPrimeSieve.java
@@ -0,0 +1,38 @@
+public class EratosthenesPrimeSieve implements PrimeSieve{
+
+    private int obergrenze;
+
+    public EratosthenesPrimeSieve(int obergrenze) {
+        this.obergrenze = obergrenze;
+    }
+
+    @Override
+    public boolean isPrime(int p) {
+        if(p < 2){
+            return false;
+        } else if (p == 2) {
+            return true;
+        }else if(p%2 == 0){
+            return false;
+        }else{
+            for (int i = 3;i <= Math.sqrt(p);i += 2){
+                if(p%i == 0){
+                    return false;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public void printPrimes() {
+        for(int i = 0;i <= obergrenze;i++){
+            if(isPrime(i)){
+                System.out.println(i);
+            }
+        }
+    }
+
+    
+}
